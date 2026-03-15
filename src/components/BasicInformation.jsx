9ee9a12a3ad0dff7dc89d48778424ac26d89c23e
smileywrld <ismaileyyunusa@gmail.com>
2026-03-15 11:11:19 +0100

    styling the first form

--- src/components/BasicInformation.jsx
+++ src/components/BasicInformation.jsx
@@ -22,6 +22,15 @@ const BasicInformation = () => {
 				<p className="text-gray-500">Questions 1 - 10</p>
 				<hr className="border-[#F3F4F6] mt-3"></hr>
 			</div>
+
+			<form autoComplete="off" className="flex">
+				<div className="flex flex-col">
+					<label htmlFor="caseID" className="text-[#364153]">
+						Case ID <span className="text-red-600">*</span>
+					</label>
+					<input type="text" className="border"/>
+				</div>
+			</form>
 		</div>
 	);
 };
